a90d58c33f4ecfff52d486eb59831ccde4a84d7b
h82258652 <842053625@qq.com>
2014-03-07 10:55:53 +0800

    update
    去除旧的Ini.cs

--- Common/Test/Program.cs
+++ Common/Test/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
-using System.Web.Script.Serialization;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using System.IO;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace Test
 {
@@ -19,8 +19,6 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Common.Config.IniConfigHelper.Set(@"d:\xmltest\bb\aa.ini", "App", "zzzz", 1);
-            Console.WriteLine(Common.Config.IniConfigHelper.Get(@"d:\xmltest\aa.ini", "App", "zzzz"));
             Console.ReadKey();
         }
     }
