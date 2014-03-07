7f74be4367bb6b2855e1c630d692cab63744b775
h82258652 <842053625@qq.com>
2014-03-07 16:00:21 +0800

    update
    添加注释

--- Common/Common/Threading/Action/ThreadHelperResult.Action.cs
+++ Common/Common/Threading/Action/ThreadHelperResult.Action.cs
@@ -8,12 +8,19 @@ namespace Common.Threading
             HasFinish = false;
         }
 
+        /// <summary>
+        /// 指示方法是否结束
+        /// </summary>
         public bool HasFinish
         {
             get;
             internal set;
         }
 
+        /// <summary>
+        /// 等待方法结束
+        /// </summary>
+        /// <returns></returns>
         public ThreadHelperResult WaitForFinish()
         {
             while (true)
