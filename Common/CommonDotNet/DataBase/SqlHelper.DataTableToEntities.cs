97c723b05d7c31356e02cd7b04d9302c27832787
h82258652 <842053625@qq.com>
2014-04-11 11:18:04 +0800

    update
    update SqlHelper

---
+++ Common/CommonDotNet/DataBase/SqlHelper.DataTableToEntities.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace Common.DataBase
+{
+    public partial class SqlHelper
+    {
+        /// <summary>
+        /// 将数据表反射到对象实体集合。
+        /// </summary>
+        /// <typeparam name="T">对象实体的类型。</typeparam>
+        /// <param name="table">数据表。</param>
+        /// <returns>对象实体集合。</returns>
+        public static IList<T> DataTableToEntities<T>(DataTable table)
+        {
+            List<T> list = new List<T>();
+            foreach (DataRow row in table.Rows)
+            {
+                T entity = DataRowToEntity<T>(row);
+                list.Add(entity);
+            }
+            return list;
+        }
+    }
+}
