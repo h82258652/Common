89050ab700decc619fc194f8cda65567d71d3c5f
h82258652 <842053625@qq.com>
2014-04-30 16:01:44 +0800

    update
    add js

---
+++ Common/CommonJS/System/Array.distinct.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+    Array.prototype.distinct = Array.prototype.distinct || function () {
+        var arr = [];
+        for (var i = 0; i < this.length; i++) {
+            if (arr.indexOf(this[i]) < 0) {
+                arr.push(this[i]);
+            }
+        }
+        return arr;
+    };
+}());
