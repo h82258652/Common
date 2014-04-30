f171d6c9d12bfcd6252d012489c3f60ed06eaa7d
h82258652 <842053625@qq.com>
2014-04-30 17:02:04 +0800

    fixed
    fix array.distinct

--- Common/CommonJS/System/Array.distinct.js
+++ Common/CommonJS/System/Array.distinct.js
@@ -3,7 +3,14 @@
     Array.prototype.distinct = Array.prototype.distinct || function () {
         var arr = [];
         for (var i = 0; i < this.length; i++) {
-            if (arr.indexOf(this[i]) < 0) {
+            var hasInclude = false;
+            for (var j = 0; j < arr.length; j++) {
+                if (arr[j] === this[i]) {
+                    hasInclude = true;
+                    break;
+                }
+            }
+            if (hasInclude === false) {
                 arr.push(this[i]);
             }
         }
