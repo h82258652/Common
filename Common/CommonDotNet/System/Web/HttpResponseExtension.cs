23414c2cc8ea23dc9d6939e6b7e3180b23c283c3
h82258652 <842053625@qq.com>
2014-05-28 14:06:49 +0800

    update
    add a extension.

---
+++ Common/CommonDotNet/System/Web/HttpResponseExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using Common.Serialization.Json;
+
+namespace Common.System.Web
+{
+    /// <summary>
+    /// HttpResponse 扩展类。
+    /// </summary>
+    public static partial class HttpResponseExtension
+    {
+        /// <summary>
+        /// 将对象 JSON 序列化，写入 HTTP 响应输出流。
+        /// </summary>
+        /// <param name="response">HTTP 响应输出流。</param>
+        /// <param name="obj">写入的对象。</param>
+        /// <exception cref="System.ArgumentNullException"><c>response</c> 为 null。</exception>
+        public static void WriteJson(this HttpResponse response, object obj)
+        {
+            if (response == null)
+            {
+                throw new ArgumentNullException("response");
+            }
+            response.Write(JsonHelper.SerializeToJson(obj));
+        }
+    }
+}
